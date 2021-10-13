d4560e7d83de5d4d632fb0c1bb495813f8c00ea2
WillianX47 <willianx47@gmail.com>
2021-10-13 20:13:37 -0300

    [Feature Will - Task: Hotfix | 13.10.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -34,8 +35,13 @@ public class Postagem {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataPostagem = new java.sql.Date(System.currentTimeMillis());
-
-	private @ManyToOne @JsonIgnoreProperties("postagem") Tema temaPostagem;
+	
+	@ManyToOne
+	@JoinColumn(name = "tema_id")
+	@JsonIgnoreProperties({"postagem"}) 
+	private Tema temaPostagem;
+	
+	
 
 	public Tema getTemaPostagem() {
 		return temaPostagem;
