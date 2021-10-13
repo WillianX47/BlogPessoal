2c126b534bea0e5ed0459c1be6dfdb3ba44e1225
WillianX47 <willianx47@gmail.com>
2021-10-13 20:19:41 -0300

    [Feature Will - Task: Link Usuario/Postagem | 13.10.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,12 +1,19 @@
 package org.generation.blogPessoal.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -20,7 +27,9 @@ public class Usuario {
 	
 	private @NotBlank @Size(min = 5, max = 100) String senha;
 	
-	
+	@OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"criador"})
+	private List<Postagem> postagens = new ArrayList<>();
 
 	public Long getId() {
 		return id;
