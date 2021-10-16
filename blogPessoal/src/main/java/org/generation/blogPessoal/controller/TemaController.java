c00dbb753b014313a19cf21bfac9ef6b013dd0c8
WillianX47 <willianx47@gmail.com>
2021-10-15 21:59:58 -0300

    [Feature Will - Task: Manutenção TemaController | 15.10.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -19,11 +19,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
+@Api(tags = "Controlador de tema", description = "Utilitário de tema")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/tema")
 public class TemaController {
