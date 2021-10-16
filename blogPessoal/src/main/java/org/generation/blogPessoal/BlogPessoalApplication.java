c291449ba106210128626af5021479c7f7269ea8
WillianX47 <willianx47@gmail.com>
2021-10-16 00:21:25 -0300

    [Feature Will - Task: Manutenção | 16.10.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
@@ -2,6 +2,7 @@ package org.generation.blogPessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class BlogPessoalApplication {
 
+	@GetMapping
 	public ModelAndView swaggerUi() {
 		return new ModelAndView("redirect:/swagger-ui/");
 	}
