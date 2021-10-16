fcb27bea36712c6ebf2018171e41b52cb777b49d
WillianX47 <willianx47@gmail.com>
2021-10-15 22:12:43 -0300

    [Feature Will - Task: Manutenção SwaggerConfig | 15.10.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
@@ -22,17 +22,14 @@ public class SwaggerConfig {
 	 * Define a package onde estão as classes do tipo @RestController, para que o
 	 * Swagger mapeie todas as classes e seus respectivos endpoints para montar a
 	 * documentação do projeto.
-	 * 
+	 * @author Will
 	 * @return
 	 */
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("org.generation.blogPessoal.controller"))
-				.paths(PathSelectors.any()).build().apiInfo(metadata()).useDefaultResponseMessages(false)
-				.globalResponses(HttpMethod.GET, responseMessage()).globalResponses(HttpMethod.POST, responseMessage())
-				.globalResponses(HttpMethod.PUT, responseMessage())
-				.globalResponses(HttpMethod.DELETE, responseMessage());
+				.paths(PathSelectors.any()).build().apiInfo(metadata()).useDefaultResponseMessages(false);
 	}
 
 	/**
@@ -43,6 +40,7 @@ public class SwaggerConfig {
 	 * (.licenseUrl)Informa o link de acesso da licença da sua aplicação (geralmente se aplica a licença no
 	 *	Github).
 	 *	Define os dados para contato com o desenvolvedor inseridos no método contact().
+	 * @author Will
 	 * @return
 	 */
 	public static ApiInfo metadata() {
@@ -63,9 +61,10 @@ public class SwaggerConfig {
 	 * Define as mensagens personalizadas para os códigos de Resposta do protocolo http (http
 	 * Response) para todos os verbos (GET, POST, PUT e DELETE). Cada linha é referente a um
 	 * Status Code.
+	 * @author Will
 	 * @return
 	 */
-	private static List<Response> responseMessage() {
+	/*private static List<Response> responseMessage() {
 		return new ArrayList<Response>() {
 			private static final long serialVersionUID = 1L;
 			{
@@ -78,5 +77,5 @@ public class SwaggerConfig {
 				add(new ResponseBuilder().code("500").description("Erro!").build());
 			}
 		};
-	}
+	}*/
 }
