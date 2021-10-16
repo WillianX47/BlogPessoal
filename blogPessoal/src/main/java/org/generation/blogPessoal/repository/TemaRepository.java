8a96ed73b94d38b8fb98780c884432a2a5899a17
WillianX47 <willianx47@gmail.com>
2021-10-15 22:57:37 -0300

    [Feature Will - Task: Documentação TemaRepository | 15.10.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
@@ -5,10 +5,24 @@ import java.util.Optional;
 
 import org.generation.blogPessoal.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 
+	/**
+	 * Procura todas as postagens encontradas pela descrição
+	 * @param descricaoTema
+	 * @return lista de postagens encontradas pela descrição
+	 * @author Will
+	 */
 	public List<Tema> findAllByDescricaoTemaContainingIgnoreCase(String descricaoTema);
 	
+	/**
+	 * Procura todos os temas pela descrição
+	 * @param descricaoTema
+	 * @return Objeto Optional de postagem encontado pela descrição
+	 * @author Will
+	 */
 	public Optional<Tema> findByDescricaoTemaContainingIgnoreCase(String descricaoTema);
 }
