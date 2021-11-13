34414fcb7612af287b6279602b6e8e152e28a48b
WillianX47 <willianx47@gmail.com>
2021-11-13 19:09:38 -0300

    [Feature Will - Task: Put Usuario | 13.11.21]

--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -35,6 +35,22 @@ public class UsuarioService {
 			return ResponseEntity.ok(objetoLista);
 		}
 	}
+	
+	public ResponseEntity<Usuario> atualizarCadastro(Usuario usuario){
+		return repository.findById(usuario.getId()).map(resp -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+			resp.setNome(usuario.getNome());
+			resp.setUsuario(usuario.getUsuario());
+			resp.setSenha(usuario.getSenha());
+			resp.setFoto(usuario.getFoto());
+			resp.setTipo(usuario.getTipo());
+			return ResponseEntity.ok(repository.save(resp));
+		}).orElseGet(() -> {
+			return ResponseEntity.badRequest().build();
+		});
+	}
 
 	/**
 	 * Método utilizado para cadastrar um usuário no sistema, verifica se o usuário
